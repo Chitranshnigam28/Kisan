2b330899c6695c685c2652cb2db52f6f26caddd6
chitransh nigam <chitranshnigam28@gmail.com>
2024-10-10 18:28:57 -0700

    modifications done

--- frontend/src/Components/CropPriceGraph.jsx
+++ frontend/src/Components/CropPriceGraph.jsx
@@ -3,19 +3,22 @@ import Chart from 'react-apexcharts';
 
 const CropPriceChart = ({ crop }) => {
     const [cropPrices, setCropPrices] = useState([]);
+    const [cropName,setCropName]=useState('');
     const [loading, setLoading] = useState(true);
 
     // Fetch crop prices from the backend
     const fetchCropPrices = async () => {
         try {
-            const response = await fetch(`http://localhost:5001/api/crops/price?crop=Wheat`);
+            const cropSent=cropName?cropName:'Wheat';
+            console.log("cropSent "+cropSent);
+            const response = await fetch(`http://localhost:5001/api/crops/price?crop=${cropSent}`);
             // console.log("response.data()"+response.data());
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const data = await response.json();
 
-            console.log("data"+JSON.stringify(data));
+            console.log("data received new "+JSON.stringify(data));
             // Extract historical prices
             const historicalPrices = data[0].market_price.historical_prices;
 
@@ -52,10 +55,26 @@ const CropPriceChart = ({ crop }) => {
             }
         }
     };
-
+    const handleCropName=(e)=>{
+        const crpName=e.target.value;
+        setCropName(crpName);
+        
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setLoading(true);
+        fetchCropPrices();
+        console.log(cropName);
+    }
     return (
         <div>
+            
+            <form onSubmit={handleSubmit} className="flex flex-col m-auto w-2/3 place-items-center mt-8">
             <h1> Enter The Crop </h1>
+                <input type="text" value={cropName}onChange={handleCropName} className="p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 my-4"/>
+                <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+  >Get Prices</button>
+            </form>
             {loading ? (
                 <p>Loading...</p>
             ) : (
