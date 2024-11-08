4e6d0fb8b8d208d66f3067e860f78db501b8c2d8
Falgunisaini <falgunisaini675@gmail.com>
2024-11-07 19:45:09 -0800

    weather changes

--- frontend/src/Components/SingleTips.jsx
+++ frontend/src/Components/SingleTips.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../css/tipsSlider.css'; 
+// import '../css/tipsSlider.css'; 
 
 const SingleTips = ({ cropName }) => {
   const [tipsData, setTipsData] = useState([]);
