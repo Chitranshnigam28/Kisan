72757f29dc11d3dc7060f19c1dee2285a2b112e8
Falgunisaini <falgunisaini675@gmail.com>
2024-11-14 20:45:56 -0800

    Line chart issue resolution by Falguni

--- frontend/src/Components/MarketInsigtsDashboard.jsx
+++ frontend/src/Components/MarketInsigtsDashboard.jsx
@@ -1,7 +1,78 @@
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { BsArrowUpRightCircle } from "react-icons/bs";
+// import CropPriceGraph from "./CropPriceGraph";
+// import { Footer } from "./Dashboard/Footer";
+// import Header from "./Dashboard/Header";
+// import { useLocation } from "react-router-dom";
+// import TopCropsChart from "./TopCropChart";
+// import TopCropIndia from "./TopCropIndia";
+// import Loader from "./CropLoader";
+// import "../css/main.css";
+// import Demo from "./Demo";
+
+// const MarketInsigtsDashboard = () => {
+//   const [location, setLocation] = useState("Delhi");
+//   const weblocation = useLocation();
+//   const [getCropAllData, setcropAllData] = useState([]);
+//   const [cropName, setCropName] = useState("Wheat"); 
+// const [selectedYear, setSelectedYear] = useState("2023"); 
+
+
+//   const handleLocationChange = (newLocation) => {
+//     setLocation(newLocation);
+//     console.log(`Location changed to: ${newLocation}`);
+//   };
+
+//   const handlegetCropdata = (data)=>{
+//     console.log(data,"chota --------------------------------")
+//     setcropAllData(data)
+//   }
+
+//   return (
+//     <>
+//       <div className="market-section-container">
+//         {weblocation.pathname === "/" ? (
+//           <>
+//           <Link to="charts" className="LinkToCharts">
+//             <BsArrowUpRightCircle style={{ fontSize: "30px", color: "grey", float: "right" }} />
+//           </Link>
+//             <div className="topCropIndia-container">
+//               <TopCropIndia />
+//             </div>
+//             <div className="topCropChart-container">
+//               <TopCropsChart
+//                 onLocationChange={handleLocationChange}
+//                 location={location}
+//               />
+//             </div>
+//           </>
+//         ) : (
+//           <div className="main-market-container">
+//             <>
+//               <Header />
+//               <Footer />
+//               <div>
+//                 <CropPriceGraph crop="wheat" getCropData = {handlegetCropdata} />
+//                 {getCropAllData && getCropAllData.length>0  ? <Demo  crops = {getCropAllData} cropName={cropName} selectedYear={selectedYear}/>:<></>}
+                
+//                 <TopCropsChart />
+//               </div>
+//             </>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default MarketInsigtsDashboard;
+
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BsArrowUpRightCircle } from "react-icons/bs";
-import CropPriceGraph from "./CropPriceGraph";
+import CropPriceChart from "./CropPriceGraph"; 
 import { Footer } from "./Dashboard/Footer";
 import Header from "./Dashboard/Header";
 import { useLocation } from "react-router-dom";
@@ -9,31 +80,40 @@ import TopCropsChart from "./TopCropChart";
 import TopCropIndia from "./TopCropIndia";
 import Loader from "./CropLoader";
 import "../css/main.css";
-import Demo from "./Demo";
 
 const MarketInsigtsDashboard = () => {
   const [location, setLocation] = useState("Delhi");
   const weblocation = useLocation();
-  const [getCropAllData, setcropAllData] = useState([])
+  const [getCropAllData, setcropAllData] = useState([]);
+  const [cropName, setCropName] = useState("Wheat"); 
+  const [selectedYear, setSelectedYear] = useState("2023"); 
 
   const handleLocationChange = (newLocation) => {
     setLocation(newLocation);
     console.log(`Location changed to: ${newLocation}`);
   };
 
-  const handlegetCropdata = (data)=>{
-    console.log(data,"chota --------------------------------")
-    setcropAllData(data)
-  }
+  const handlegetCropdata = (data) => {
+    console.log(data, "chota --------------------------------");
+    setcropAllData(data);
+  };
+
+  const handleCropNameChange = (newCropName) => {
+    setCropName(newCropName); 
+  };
+
+  const handleYearChange = (newYear) => {
+    setSelectedYear(newYear); 
+  };
 
   return (
     <>
       <div className="market-section-container">
         {weblocation.pathname === "/" ? (
           <>
-          <Link to="charts" className="LinkToCharts">
-            <BsArrowUpRightCircle style={{ fontSize: "30px", color: "grey", float: "right" }} />
-          </Link>
+            <Link to="charts" className="LinkToCharts">
+              <BsArrowUpRightCircle style={{ fontSize: "30px", color: "grey", float: "right" }} />
+            </Link>
             <div className="topCropIndia-container">
               <TopCropIndia />
             </div>
@@ -50,9 +130,13 @@ const MarketInsigtsDashboard = () => {
               <Header />
               <Footer />
               <div>
-                <CropPriceGraph crop="wheat" getCropData = {handlegetCropdata} />
-                {getCropAllData && getCropAllData.length>0  ? <Demo  crops = {getCropAllData}/>:<></>}
-                
+                <CropPriceChart 
+                  getCropData={handlegetCropdata}
+                  cropName={cropName}
+                  selectedYear={selectedYear}
+                  onCropNameChange={handleCropNameChange}
+                  onYearChange={handleYearChange}
+                />
                 <TopCropsChart />
               </div>
             </>
@@ -64,3 +148,4 @@ const MarketInsigtsDashboard = () => {
 };
 
 export default MarketInsigtsDashboard;
+
